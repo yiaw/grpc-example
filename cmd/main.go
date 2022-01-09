353f49f0de66cdf2ea94626d296ac4f7236b80ac
unknown <lsmin0703@gmail.com>
2022-01-09 19:30:18 +0900

    :recycle: app package 변경

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,7 @@ import (
 	"net"
 	"net/http"
 
-	"github.com/yiaw/grpc-example/cmd/app"
+	mgrpc "github.com/yiaw/grpc-example/cmd/grpc"
 )
 
 func main() {
@@ -22,7 +22,7 @@ func main() {
 		log.Fatalf("failed to listen; %s\n", err.Error())
 	}
 
-	s, err := app.NewGRPCServer(*tlsenable)
+	s, err := mgrpc.NewGRPCServer(*tlsenable)
 	if err != nil {
 		log.Fatalf("failed New GRPCServer() .. err=%s\n", err.Error())
 	}
@@ -33,7 +33,7 @@ func main() {
 		log.Fatalln(s.Serve(lis))
 	}()
 
-	gw, err := app.NewGateWay(*grpcport)
+	gw, err := mgrpc.NewGateWay(*grpcport)
 	if err != nil {
 		log.Fatalf("failed NewGateWay() .. err=%s\n", err.Error())
 	}
